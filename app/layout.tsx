dbee9924951dc67b4e53db75e39b022c811f0fd5
Eng Mohamed Wageh <142535559+wagehii@users.noreply.github.com>
2026-06-18 10:58:25 +0300

    Update metadata for portfolio and add keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "Mohamed Wageh | Data Scientist & AI Engineer",
-  description: "Portfolio of Mohamed Wageh — Data Scientist, AI Engineer, and CS Undergraduate at Mansoura University.",
-  icons: {
-    icon: "/favicon.ico",
+
+export const metadata = {
+  title: 'Mohamed Wageh | Portfolio',
+  description: 'البورتفوليو الرسمي لمحمد وجيه (Mohamed Wageh)، مهندس تعلم آلة وعالم بيانات متخصص في الذكاء الاصطناعي، الرؤية الحاسوبية، وتطوير نماذج بايثون.',
+  keywords: 'محمد وجيه, بورتفوليو محمد وجيه, محمد وجيه ابو سليم, Mohamed Wageh, Mohamed Wageh Abou Selim, wagehii, Zain, زين, Mohamed Wageh Portfolio, Machine Learning Engineer Egypt, Data Scientist, مهندس ذكاء اصطناعي, عالم بيانات, مهندس تعلم آلة, مطور بايثون, Mansoura University, جامعة المنصورة, حاسبات ومعلومات المنصورة, Computer Vision, YOLO, Python Developer, C++, Deep Learning, الذكاء الاصطناعي, ODESDA, DEPI, رواد مصر الرقمية, CAT Reloaded, Freelance ML Engineer',
+  authors: [{ name: 'Mohamed Wageh' }] ,
+   icons: {
+    icon: "/icon.png",
   },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
